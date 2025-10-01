981a3f313721dca44a38234158f8641cb121e30f
AMANDA FERNANDA LOPES MARTINS <17amandamartins@gmail.com>
2025-10-01 18:39:20 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home(){
+    return(
+    <div>
+        <h1>Conselhos para vida</h1>
+        <h4> 1° - Vai dar certo! </h4>
+        <h4> 2° - veja pelo lado </h4>
+        <h4> 3° - É um absurdo, mas faz sentido</h4>
+    </div>
+    )
+
+}
+
+export default Home;
