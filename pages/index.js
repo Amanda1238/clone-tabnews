585cd1b47db74bf84088d984efc734d35945be45
AMANDA FERNANDA LOPES MARTINS <17amandamartins@gmail.com>
2025-10-01 19:59:55 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return(
     <div>
-        <h1>Conselhos para vida</h1>
+        <h1>Conselhos para vida. 😒</h1>
         <h4> 1° - Vai dar certo! </h4>
         <h4> 2° - veja pelo lado </h4>
         <h4> 3° - É um absurdo, mas faz sentido</h4>
